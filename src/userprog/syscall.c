bc3a55db5b277eb480808a06f8df3e4c397ca8af
zhengxiang94 <164752618@qq.com>
2022-11-19 12:07:02 +0000

    syscall write(stdout) and practice

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -20,10 +20,25 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
    */
 
   /* printf("System call number: %d\n", args[0]); */
-
-  if (args[0] == SYS_EXIT) {
+  if (args[0] == SYS_HALT) {
+    shutdown_power_off();
+  } else if (args[0] == SYS_EXIT) {
     f->eax = args[1];
     printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
     process_exit();
+  } else if (args[0] == SYS_WRITE) {
+    int fd = args[1];
+    const char *buffer = (const char*)args[2];
+    size_t size = args[3];
+
+    if (fd == 1) {
+      putbuf((const char*)buffer, size);
+      f->eax = args[3];
+    } else {
+      //int size = write(args[1], (void*)args[2], args[3]);
+      //f->eax = size;
+    }
+  } else if (args[0] == SYS_PRACTICE) {
+    f->eax = (int)args[1] + 1;
   }
 }
