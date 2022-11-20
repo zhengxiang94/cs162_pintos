fac331aa2db6cb701a17eb1303c365371e2879d3
zhengxiang94 <164752618@qq.com>
2022-11-20 09:19:08 +0000

    syscall refactor

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,24 @@
 #include "threads/thread.h"
 #include "userprog/process.h"
 
+static void syscall_exit(struct intr_frame* f, int status) {
+  f->eax = status;
+  printf("%s: exit(%d)\n", thread_current()->pcb->process_name, status);
+  process_exit();
+}
+
+static void syscall_create(struct intr_frame* f, const char* file, unsigned initial_size) {
+  f->eax = filesys_create(file, initial_size);
+}
+
+static void syscall_write(struct intr_frame* f, int fd, const void* buffer, unsigned size) {
+  if (fd == 1) {
+    putbuf((const char*)buffer, size);
+    f->eax = size;
+    return;
+  } 
+}
+
 static void syscall_handler(struct intr_frame*);
 
 void syscall_init(void) { intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall"); }
@@ -20,25 +38,50 @@ static void syscall_handler(struct intr_frame* f UNUSED) {
    */
 
   /* printf("System call number: %d\n", args[0]); */
-  if (args[0] == SYS_HALT) {
-    shutdown_power_off();
-  } else if (args[0] == SYS_EXIT) {
-    f->eax = args[1];
-    printf("%s: exit(%d)\n", thread_current()->pcb->process_name, args[1]);
-    process_exit();
-  } else if (args[0] == SYS_WRITE) {
-    int fd = args[1];
-    const char *buffer = (const char*)args[2];
-    size_t size = args[3];
-
-    if (fd == 1) {
-      putbuf((const char*)buffer, size);
-      f->eax = args[3];
-    } else {
-      //int size = write(args[1], (void*)args[2], args[3]);
-      //f->eax = size;
+  switch (args[0]) {
+    case SYS_HALT:
+      shutdown_power_off();
+      break;
+    case SYS_EXIT:
+      syscall_exit(f, args[1]);
+      break;
+    case SYS_EXEC: {
+      break;
     }
-  } else if (args[0] == SYS_PRACTICE) {
-    f->eax = (int)args[1] + 1;
+    case SYS_WAIT: {
+      break;
+    }
+    case SYS_CREATE:
+      syscall_create(f, args[1], args[2]);
+      break;
+    case SYS_REMOVE: {
+      break;
+    }
+    case SYS_OPEN: {
+      break;
+    }
+    case SYS_FILESIZE: {
+      break;
+    }
+    case SYS_READ: {
+      break;
+    }
+    case SYS_WRITE:
+      syscall_write(f, args[1], args[2], args[3]);
+      break;
+    case SYS_SEEK: {
+      break;
+    }
+    case SYS_TELL: {
+      break;
+    }
+    case SYS_CLOSE: {
+      break;
+    }
+    case SYS_PRACTICE:
+      f->eax = (int)args[1] + 1;
+      break;
+    default:
+      break;
   }
 }
